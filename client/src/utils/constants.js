022f1cf72d81d98af635763c10d012a8507e6003
joonaramo <joona00ramo@gmail.com>
2021-08-18 11:45:00 +0300

    frontend: change api url depending on NODE_ENV

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,6 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://whispering-waters-22674.herokuapp.com/api';
 
 export { API_URL };
