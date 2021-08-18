f878ad48fee78e308728f98a0b74e2d8b7d111f9
joonaramo <joona00ramo@gmail.com>
2021-08-18 14:57:15 +0300

    add admin panel to mobile navbar

--- client/src/components/Header/Navbar.js
+++ client/src/components/Header/Navbar.js
@@ -275,6 +275,12 @@ const Navbar = ({
               >
                 Find us
               </a>
+              <Link
+                to='/admin'
+                className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
+              >
+                Admin Panel
+              </Link>
             </div>
             <div className='pt-4 pb-3 border-t border-gray-700'>
               <div className='flex items-center px-5'>
